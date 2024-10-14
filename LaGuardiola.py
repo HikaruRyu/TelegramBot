540dbac302c304d5cf7e84d46c51e458e38e97af
KOHAKU <acalle@inscastellet.cat>
2024-10-14 11:03:37 +0200

    e

--- LaGuardiola.py
+++ LaGuardiola.py
@@ -1,14 +1,10 @@
 from typing import Final
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-
-
-
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, CallbackContext
 
 # Declaración de constantes
 Token: Final = '7725937567:AAHohQsxDB1lO9-_MZX_rQ1lgnAMM095C1E'  # Token del bot
 BOT_USERNAME: Final = '@LaGuardiola_Bot'  # Nombre del Bot
-
 
 # Variables globales
 salary = 0  # Saldo actual
@@ -25,68 +21,20 @@ esperando_gastos_diaris = False  # Control de espera para los gastos diarios
 esperando_ganancies_esporadiques = False  # Control de espera para las ganancias esporádicas
 esperando_ganancies_mensuals = False  # Control de espera para las ganancias mensuales
 
-
-
-
-
-
 # Comando /start
 async def start_(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global salary, ingresos, estalvi, despeses, quotes, gastos_diaris, ganancies_esporadiques, ganancies_mensuals
-
-
-
 
     # Inicialización de variables
     estalvi = 0
     despeses = quotes + gastos_diaris
     ingresos = ganancies_esporadiques + ganancies_mensuals
-    salary = ingresos - despeses
-
-
-
+    salary = ingresos - despeses - estalvi
 
     await update.message.reply_text('¡Hola! ¿Cómo puedo ayudarte?')
 
-
-
-
-# Comando /reserva
-async def reserva_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global estalvi
-    await update.message.reply_text(f'Tienes {estalvi}€ reservados para el ahorro.')
-
-
-
-
-async def introducir_reserva_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global esperando_reserva
-    esperando_reserva = True
-    await update.message.reply_text('Introduce la cantidad que deseas reservar como ahorro.')
-
-
-
-
-async def manejar_reserva_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global estalvi, esperando_reserva
-
-
-
-
-    if esperando_reserva:
-        try:
-            quantitat = float(update.message.text.replace('€', '').strip())
-            estalvi += quantitat  # Sumar a estalvi
-            esperando_reserva = False
-            await update.message.reply_text(f'Has reservado {quantitat}€ como ahorro. Total ahorrado: {estalvi}€.')
-        except ValueError:
-            await update.message.reply_text('Por favor, introduce una cantidad válida.')
-
-
-
-
 # Comando /help
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def help_command(update: Update, context: CallbackContext):
     help_text = (
         "/help\n"
         "Aquí tienes los comandos disponibles para el bot LA GUARDIOLA:\n"
@@ -104,55 +52,46 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     await update.message.reply_text(help_text)
 
+# Comando /reserva
+async def reserva_(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global estalvi
+    await update.message.reply_text(f'Tienes {estalvi}€ reservados para el ahorro.')
 
+async def introducir_reserva_(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global esperando_reserva
+    esperando_reserva = True
+    await update.message.reply_text('Introduce la cantidad que deseas reservar como ahorro.')
 
+async def manejar_reserva_(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global estalvi, esperando_reserva, salary
+
+    if esperando_reserva:
+        try:
+            quantitat = float(update.message.text.replace('€', '').strip())
+            estalvi += quantitat  # Sumar a estalvi
+            esperando_reserva = False
+            salary = ingresos - despeses - estalvi  # Recalcular salary
+            await update.message.reply_text(f'Has reservado {quantitat}€ como ahorro. Total ahorrado: {estalvi}€. Saldo actual: {salary}€.')
+        except ValueError:
+            await update.message.reply_text('Por favor, introduce una cantidad válida.')
 
 # Comando /saldo
 async def saldo_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global salary
-    await update.message.reply_text(f'Tu saldo actual es de {salary}€.')
-    global ingresos, despeses, estalvi
+    global salary, ingresos, despeses, estalvi
     salary = ingresos - despeses - estalvi
-
-
-#async def introducir_saldo_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#    global esperando_saldo
-#    esperando_saldo = True
-#    await update.message.reply_text('Introduce la cantidad que deseas agregar al saldo.')
-
-
-#async def manejar_saldo_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#    global salary, esperando_saldo
-
-
-#    if esperando_saldo:
-#        try:
-#            cantidad = float(update.message.text.replace('€', '').strip())
-#            salary += cantidad  # Sumar al saldo
-#            esperando_saldo = False
-#            await update.message.reply_text(f'Has agregado {cantidad}€ al saldo. Saldo total: {salary}€.')
-#        except ValueError:
-#            await update.message.reply_text('Por favor, introduce una cantidad válida.')
-
-
-
+    await update.message.reply_text(f'Tu saldo actual es de {salary}€.')
 
 # Comando /despeses
 async def despesses_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global quotes, gastos_diaris, despeses
+    global quotes, gastos_diaris, despeses, salary
     despeses = quotes + gastos_diaris
-    await update.message.reply_text(f'El total de tus gastos hasta el momento es de {despeses}€.')
-
-
-
+    salary = ingresos - despeses - estalvi  # Recalcular salary
+    await update.message.reply_text(f'El total de tus gastos hasta el momento es de {despeses}€. Saldo actual: {salary}€.')
 
 # Comando /quotesMensuals
 async def quotes_mensuals_(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global quotes
     await update.message.reply_text(f'Los gastos mensuales fijos son de {quotes}€.')
-
-
-
 
 # Comando /introduirQuotesMensuals
 async def introduir_quotes_mensuals_(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -160,66 +99,50 @@ async def introduir_quotes_mensuals_(update: Update, context: ContextTypes.DEFAU
     esperando_quotes = True
     await update.message.reply_text('Introduce las nuevas cuotas mensuales que quieres agregar.')
 
-
-
-
 async def manejar_quotes_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global quotes, esperando_quotes
-
+    global quotes, esperando_quotes, despeses, salary
 
     if esperando_quotes:
         try:
             cantidad = float(update.message.text.replace('€', '').strip())
             quotes = cantidad  # Actualizar las cuotas mensuales
             esperando_quotes = False
-            await update.message.reply_text(f'Has actualizado las cuotas mensuales a {cantidad}€.')
+            despeses = quotes + gastos_diaris  # Recalcular despeses
+            salary = ingresos - despeses - estalvi  # Recalcular salary
+            await update.message.reply_text(f'Has actualizado las cuotas mensuales a {cantidad}€. Saldo actual: {salary}€.')
         except ValueError:
             await update.message.reply_text('Por favor, introduce una cantidad válida.')
-
-
-
 
 # Comando /gastosDiaris
 async def gastos_diaris_(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global gastos_diaris
     await update.message.reply_text(f'Los gastos diarios hasta ahora son de {gastos_diaris}€.')
 
-
 async def introducir_gastos_diaris_(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global esperando_gastos_diaris
     esperando_gastos_diaris = True
     await update.message.reply_text('Introduce la cantidad de gastos diarios.')
 
-
 async def manejar_gastos_diaris_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global gastos_diaris, esperando_gastos_diaris
-
+    global gastos_diaris, esperando_gastos_diaris, despeses, salary
 
     if esperando_gastos_diaris:
         try:
             cantidad = float(update.message.text.replace('€', '').strip())
             gastos_diaris += cantidad  # Sumar a gastos diarios
             esperando_gastos_diaris = False
-            await update.message.reply_text(f'Has agregado {cantidad}€ a los gastos diarios. Total de gastos diarios: {gastos_diaris}€.')
+            despeses = quotes + gastos_diaris  # Recalcular despeses
+            salary = ingresos - despeses - estalvi  # Recalcular salary
+            await update.message.reply_text(f'Has agregado {cantidad}€ a los gastos diarios. Total de gastos diarios: {gastos_diaris}€. Saldo actual: {salary}€.')
         except ValueError:
             await update.message.reply_text('Por favor, introduce una cantidad válida.')
 
-
-
-
-# Comando /introduirGastosDiaris
-async def introduir_gastos_diaris_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Introduce un nuevo gasto diario.')
-
-
 # Comando /ganancies
 async def ganancies_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global ganancies_esporadiques, ganancies_mensuals
+    global ganancies_esporadiques, ganancies_mensuals, ingresos, salary
     ingresos = ganancies_esporadiques + ganancies_mensuals
-    await update.message.reply_text(f'Tus ganancias totales son de {ingresos}€.')
-
-
-
+    salary = ingresos - despeses - estalvi  # Recalcular salary
+    await update.message.reply_text(f'Tus ganancias totales son de {ingresos}€. Saldo actual: {salary}€.')
 
 # Comando /introduirGananciesEsporadiques
 async def introducir_ganancies_esporadiques_(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -227,22 +150,19 @@ async def introducir_ganancies_esporadiques_(update: Update, context: ContextTyp
     esperando_ganancies_esporadiques = True
     await update.message.reply_text('Introduce los ingresos esporádicos que quieres agregar.')
 
-
 async def manejar_ganancies_esporadiques_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global ganancies_esporadiques, esperando_ganancies_esporadiques
-
+    global ganancies_esporadiques, esperando_ganancies_esporadiques, ingresos, salary
 
     if esperando_ganancies_esporadiques:
         try:
             cantidad = float(update.message.text.replace('€', '').strip())
             ganancies_esporadiques += cantidad  # Sumar a ganancies esporadiques
             esperando_ganancies_esporadiques = False
-            await update.message.reply_text(f'Has agregado {cantidad}€ a las ganancias esporádicas. Total de ganancias esporádicas: {ganancies_esporadiques}€.')
+            ingresos = ganancies_esporadiques + ganancies_mensuals  # Recalcular ingresos
+            salary = ingresos - despeses - estalvi  # Recalcular salary
+            await update.message.reply_text(f'Has agregado {cantidad}€ a las ganancias esporádicas. Total de ganancias esporádicas: {ganancies_esporadiques}€. Saldo actual: {salary}€.')
         except ValueError:
             await update.message.reply_text('Por favor, introduce una cantidad válida.')
-
-
-
 
 # Comando /introduirGananciesMensuals
 async def introducir_ganancies_mensuals_(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -250,77 +170,55 @@ async def introducir_ganancies_mensuals_(update: Update, context: ContextTypes.D
     esperando_ganancies_mensuals = True
     await update.message.reply_text('Introduce las ganancias mensuales.')
 
-
 async def manejar_ganancies_mensuals_(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global ganancies_mensuals, esperando_ganancies_mensuals
-
+    global ganancies_mensuals, esperando_ganancies_mensuals, ingresos, salary
 
     if esperando_ganancies_mensuals:
         try:
             cantidad = float(update.message.text.replace('€', '').strip())
             ganancies_mensuals += cantidad  # Sumar a ganancies mensuals
             esperando_ganancies_mensuals = False
-            await update.message.reply_text(f'Has agregado {cantidad}€ a las ganancias mensuales. Total de ganancias mensuales: {ganancies_mensuals}€.')
+            ingresos = ganancies_esporadiques + ganancies_mensuals  # Recalcular ingresos
+            salary = ingresos - despeses - estalvi  # Recalcular salary
+            await update.message.reply_text(f'Has agregado {cantidad}€ a las ganancias mensuales. Total de ganancias mensuales: {ganancies_mensuals}€. Saldo actual: {salary}€.')
         except ValueError:
             await update.message.reply_text('Por favor, introduce una cantidad válida.')
-
-
-
 
 # Respuestas
 def handle_response(text: str) -> str:
     processed: str = text.lower()
-   
+
     if 'hello' in processed:
         return '¡Hola!'
     return 'No entiendo lo que escribiste.'
 
-
-
-
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global esperando_reserva, esperando_quotes, esperando_gastos_diaris, esperando_ganancies_esporadiques, esperando_ganancies_mensuals
-
-
-
 
     message_type: str = update.message.chat.type
     text: str = update.message.text
 
-
-
-
     print(f'User ({update.message.chat.id}) in {message_type} : "{text}"')
-
-
-
 
     if esperando_reserva:
         await manejar_reserva_(update, context)
         return
 
-
     if esperando_quotes:
         await manejar_quotes_(update, context)
         return
-
 
     if esperando_gastos_diaris:
         await manejar_gastos_diaris_(update, context)
         return
 
-
     if esperando_ganancies_esporadiques:
         await manejar_ganancies_esporadiques_(update, context)
         return
 
-
     if esperando_ganancies_mensuals:
         await manejar_ganancies_mensuals_(update, context)
         return
-
-
-
 
     if message_type == 'group':
         if BOT_USERNAME in text:
@@ -330,26 +228,17 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             return
     else:
         response: str = handle_response(text)
-   
+
     print('Bot:', response)
     await update.message.reply_text(response)
-
-
-
 
 # Error handler
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'Update {update} caused error {context.error}')
 
-
-
-
 if __name__ == '__main__':
     print('Bot is running')
     app = Application.builder().token(Token).build()
-
-
-
 
     # Comandos
     app.add_handler(CommandHandler('start', start_))
@@ -371,16 +260,9 @@ if __name__ == '__main__':
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
 
 
-
-
     # Errors
     app.add_error_handler(error)
-
-
-
 
     # Polling
     print('Bot is polling')
     app.run_polling(poll_interval=3)
-
-
